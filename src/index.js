71d58d8e6dd9900d7a7bda010cfa02380dcbd452
petushka1 <nadya@devery.io>
2022-06-28 15:22:59 +0300

    fix linters error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './css/style.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
 
-
 import renderFooter from './modules/footer.js';
 
 renderFooter();
