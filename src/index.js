0c3d66eddd5f51f6cff24667a10db0e2f231b056
petushka1 <nadya@devery.io>
2022-06-28 15:19:57 +0300

    fix bootstrap issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './css/style.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
+
+
 import renderFooter from './modules/footer.js';
 
 renderFooter();
