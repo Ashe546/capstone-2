0520462a3efe4bdc29037c43b4a2dcc249fb5762
Ashe546 <asfyd42@gmail.com>
2022-06-29 05:20:57 -0300

    Render home page dynamicaly

--- src/Module/Home.js
+++ src/Module/Home.js
@@ -1,16 +1,15 @@
 // hompage 
-const component = () => {
+const component = (name, img) => {
     const element = document.createElement('div');
-    element.className = 'row';
-      element.innerHTML += `<div class="col-3" style="height: 300px; margin-right: 100px; margin-left: 80px;">
-        <div class="row" style="height: 50%; background-color:azure align-items: center; justify-content: center;"><img style ="height: 100%; width: auto;" src="https://static.tvmaze.com/uploads/images/medium_portrait/171/428737.jpg" alt="img"></div>
-        <div class="row"><h1>Space 1 <i>heart</i></h1></div>
+    element.className = 'col-3 home-col';
+      element.innerHTML += `<div class="row" style="height: 50%; background-color:azure align-items: center; justify-content: center;">
+        <img style ="height: 100%; width: auto;" src=${img} alt="img"></div>
+        <div class="row"><h1>${name}<i>heart</i></h1></div>
         <div class="button-tags">
           <button>Comment</button>
           <button>Reservation</button>
-        </div>
-      </div>`;
+        </div>`;
     return element;
   };
 
-  export { component }
+  export { component };
--- src/index.js
+++ src/index.js
@@ -6,66 +6,76 @@ import {component} from './Module/Home'
 const movie = 'comedy';
 const url = `https://api.tvmaze.com/search/shows?q=${movie}`;
 
+console.log(url)
 
 // Render out the movies dynamicaly
-const Home = document.querySelector('.home-page')
-Home.appendChild(component());
-
-
+const Home = document.querySelector('#row')
 const displayMovies = document.querySelector('#display-movies');
+
 export async function getMovies() {
   const response = await fetch(new Request(url));
   const result = await response.json();
   return result;
 }
 
-export const popupMovieDetail = async (id) => {
-  const movies = await getMovies();
-  const detailPopup = document.createElement('div');
-  detailPopup.className = 'popup';
-  movies.forEach((movie) => {
-    if (id === movie.show.id) {
-      const movieName = document.createElement('lable');
-      movieName.innerHTML += `${movie.show.name}`;
-      const closeButton = document.createElement('button');
-      closeButton.type = 'button';
-      closeButton.innerHTML = 'close';
-      const movieStatus = document.createElement('lable');
-      movieStatus.innerHTML += `Status : ${movie.show.status}`;
-      const moviePremiered = document.createElement('lable');
-      moviePremiered.innerHTML += `Premiered : ${movie.show.premiered}`;
-      const movieImage = document.createElement('img');
-      movieImage.src = movie.show.image.medium;
-      detailPopup.append(movieName, movieImage, movieStatus, moviePremiered, closeButton);
-      document.body.append(detailPopup);
+const homeDisplay = async () => {
+    const movies = await getMovies();
+    movies.forEach((movie) => {
+        Home.appendChild(component(movie.show.name, movie.show.image.medium));
+    })
+}
 
-      closeButton.addEventListener('click', () => {
-        document.body.removeChild(detailPopup);
-      });
-    }
-  });
-};
+homeDisplay();
 
-export const display = async () => {
-  const movies = await getMovies();
-  movies.forEach((movie) => {
-    const movieList = document.createElement('div');
-    movieList.className = 'movie-list';
-    displayMovies.append(movieList);
-    const movieName = document.createElement('lable');
-    movieName.innerHTML += `${movie.show.name}`;
-    const movieImage = document.createElement('img');
-    movieImage.src = movie.show.image.medium;
-    const commentButton = document.createElement('button');
-    commentButton.type = 'button';
-    commentButton.className = 'comment';
-    commentButton.innerHTML += 'comment';
-    movieList.append(movieName, movieImage, commentButton);
 
-    commentButton.addEventListener('click', () => {
-      popupMovieDetail(movie.show.id);
-    });
-  });
-};
 
-display();
+// export const popupMovieDetail = async (id) => {
+//   const movies = await getMovies();
+//   const detailPopup = document.createElement('div');
+//   detailPopup.className = 'popup';
+//   movies.forEach((movie) => {
+//     if (id === movie.show.id) {
+//       const movieName = document.createElement('lable');
+//       movieName.innerHTML += `${movie.show.name}`;
+//       const closeButton = document.createElement('button');
+//       closeButton.type = 'button';
+//       closeButton.innerHTML = 'close';
+//       const movieStatus = document.createElement('lable');
+//       movieStatus.innerHTML += `Status : ${movie.show.status}`;
+//       const moviePremiered = document.createElement('lable');
+//       moviePremiered.innerHTML += `Premiered : ${movie.show.premiered}`;
+//       const movieImage = document.createElement('img');
+//       movieImage.src = movie.show.image.medium;
+//       detailPopup.append(movieName, movieImage, movieStatus, moviePremiered, closeButton);
+//       document.body.append(detailPopup);
+
+//       closeButton.addEventListener('click', () => {
+//         document.body.removeChild(detailPopup);
+//       });
+//     }
+//   });
+// };
+
+// export const display = async () => {
+//   const movies = await getMovies();
+//   movies.forEach((movie) => {
+//     const movieList = document.createElement('div');
+//     movieList.className = 'movie-list';
+//     displayMovies.append(movieList);
+//     const movieName = document.createElement('lable');
+//     movieName.innerHTML += `${movie.show.name}`;
+//     const movieImage = document.createElement('img');
+//     movieImage.src = movie.show.image.medium;
+//     const commentButton = document.createElement('button');
+//     commentButton.type = 'button';
+//     commentButton.className = 'comment';
+//     commentButton.innerHTML += 'comment';
+//     movieList.append(movieName, movieImage, commentButton);
+
+//     commentButton.addEventListener('click', () => {
+//       popupMovieDetail(movie.show.id);
+//     });
+//   });
+// };
+
+// display();
