bdd34197075f5eead11759d0e9d8a259b757fc73
Ashe546 <asfyd42@gmail.com>
2022-06-30 12:01:05 -0300

    Fixed linter error

--- webpack.config.js
+++ webpack.config.js
@@ -35,20 +35,6 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              // Prefer `dart-sass`
-              implementation: require('sass'),
-            },
-          },
-        ],
-      },
     ],
   },
 };
